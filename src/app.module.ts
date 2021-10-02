bf17d8ea51706919a9681ec0c0468841ae5c8de3
Hansanguk <hansanguk0222@gmail.com>
2021-10-02 12:15:07 +0900

    [feat] 학습자료 업로드

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './logger.middleware';
+@Module({
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    throw new HttpException('api broken', 401);
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param, typeof param);
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update cat';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete cat';
+  }
+}
